dd0691094c37f5e14d072c1a9220c262e0570a1e
eScheurer <enya.scheurer@quickline.ch>
2025-12-28 22:39:58 +0100

    sorts display of results by count and alphabetically

--- java-spring/src/main/resources/static/result.js
+++ java-spring/src/main/resources/static/result.js
@@ -5,15 +5,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const answers = Array.from(answerElements).map(element => ({
         uid: element.dataset.uid,
-        content: element.dataset.content
+        content: element.dataset.content,
+        count: 0
     }));
 
-    answers.forEach(answer => {
+    const fetches = answers.map(answer =>
         fetch(`/answers/${answer.uid}/count`)
             .then(response => response.text())
             .then(count => {
-                render(answer.content, count);
-            });
+                answer.count = parseInt(count, 10) || 0;
+            })
+    );
+
+    // have to wait for all to be fetched to be able to sort
+    Promise.all(fetches).then(() => {
+        answers.sort((a,b) => b.count - a.count || a.content.localeCompare(b.content)); //localCompare = alphabetical sorting
+
+        answers.forEach(answer => {
+            render(answer.content, answer.count);
+        })
     });
 });
 
