23a181cc9ba0738deb418fcdb75e0acde459060a
Jannick <j.seper@stud.unibas.ch>
2026-01-29 20:07:00 +0100

    still works

--- java-spring/src/main/java/quickstart/service/Game.java
+++
@@ -1,8 +0,0 @@
-package quickstart.service;
-
-import jakarta.annotation.Nonnull;
-
-public record Game(
-        @Nonnull String id,
-        @Nonnull String status
-) { }
--- java-spring/src/main/java/quickstart/service/Player.java
+++
@@ -1,8 +0,0 @@
-package quickstart.service;
-
-import jakarta.annotation.Nonnull;
-
-public record Player(
-        @Nonnull String pid,
-        @Nonnull String ip
-) { }
