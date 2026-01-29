39db7e6279b977fcf19378928c42cca5238fa2f9
Jannick <j.seper@stud.unibas.ch>
2026-01-29 20:15:13 +0100

    still works

--- java-spring/src/main/java/khitto/configuration/DittoConfigurationKeys.java
+++ java-spring/src/main/java/khitto/configuration/DittoConfigurationKeys.java
@@ -1,4 +1,4 @@
-package quickstart.configuration;
+package khitto.configuration;
 
 public class DittoConfigurationKeys {
     public static final String DITTO_DIR = "ditto.dir";
--- java-spring/src/main/java/khitto/service/DittoService.java
+++ java-spring/src/main/java/khitto/service/DittoService.java
@@ -24,7 +24,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-import quickstart.configuration.DittoConfigurationKeys;
+import khitto.configuration.DittoConfigurationKeys;
 import reactor.core.publisher.Sinks;
 
 @Component
